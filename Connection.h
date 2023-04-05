4f85b97ecda1f2a3e72d26ab268f66500d5b7564
Lucas Nakajima <nakajima.lucas02@gmail.com>
2023-04-05 12:23:08 +0100

    Revert "Doxygen"
    This reverts commit 24793a4932b2b258887d048ff59e1b8ca0caaab1.

--- Connection.h
+++ Connection.h
@@ -10,112 +10,36 @@
 
 using namespace std;
 
-/**
-
-@brief The Connection class represents a connection between two stations in a transportation network.
-*/
 class Connection {
 private:
-    Station source; ///< The source station of the connection.
-    Station destination; ///< The destination station of the connection.
-    int capacity; ///< The maximum flow that can be passed through the connection.
-    string service; ///< The name of the transportation service associated with the connection.
-    int residual; ///< The remaining capacity of the connection.
+    Station source;
+    Station destination;
+    int capacity;
+    string service;
+    int residual;
 
 public:
-/**
-* @brief Default constructor for the Connection class.
-*
-*/
     Connection();
-/**
- * @brief Constructor for the Connection class.
- *
- * @param source The source station of the connection.
- * @param destination The destination station of the connection.
- * @param capacity The maximum flow that can be passed through the connection.
- * @param service The name of the transportation service associated with the connection.
- */
-Connection(Station source, Station destination, int capacity, string service);
 
-/**
- * @brief Overloaded assignment operator for the Connection class.
- *
- * @param other Pointer to the Connection object to be assigned.
- * @return Connection& The Connection object after assignment.
- */
-Connection& operator=(Connection *other);
+    Connection(Station source, Station destination, int capacity, string service);
 
-/**
- * @brief Getter function for the source station of the connection.
- *
- * @return Station The source station of the connection.
- */
-Station getSource() const;
+    Connection& operator=(Connection *other);
 
-/**
- * @brief Setter function for the source station of the connection.
- *
- * @param newSource The new source station of the connection.
- */
-void setSource(Station newSource);
+    Station getSource() const;
+    void setSource(Station newSource);
 
-/**
- * @brief Getter function for the destination station of the connection.
- *
- * @return Station The destination station of the connection.
- */
-Station getDestination() const;
+    Station getDestination() const;
+    void setDestination(Station newDestination);
 
-/**
- * @brief Setter function for the destination station of the connection.
- *
- * @param newDestination The new destination station of the connection.
- */
-void setDestination(Station newDestination);
+    int getCapacity() const;
+    void setCapacity(int newCapacity);
 
-/**
- * @brief Getter function for the maximum flow that can be passed through the connection.
- *
- * @return int The maximum flow that can be passed through the connection.
- */
-int getCapacity() const;
+    string getService() const;
+    void setService(string newService);
 
-/**
- * @brief Setter function for the maximum flow that can be passed through the connection.
- *
- * @param newCapacity The new maximum flow that can be passed through the connection.
- */
-void setCapacity(int newCapacity);
-
-/**
- * @brief Getter function for the name of the transportation service associated with the connection.
- *
- * @return string The name of the transportation service associated with the connection.
- */
-string getService() const;
-
-/**
- * @brief Setter function for the name of the transportation service associated with the connection.
- *
- * @param newService The new name of the transportation service associated with the connection.
- */
-void setService(string newService);
-
-/**
- * @brief Getter function for the remaining capacity of the connection.
- *
- * @return int The remaining capacity of the connection.
- */
-int getResidual();
-
-/**
- * @brief Setter function for the remaining capacity of the connection.
- *
- * @param newresidual The new remaining capacity of the connection.
- */
-void setResidual(int newresidual);
-
+    int getResidual();
+    void setResidual(int newresidual);
+};
 
 
 #endif //PROJETO_CONNECTION_H
--- Graph.h
+++ Graph.h
@@ -28,235 +28,55 @@ private:
 public:
     Graph();
 
-    /**
-     @brief Loads the station information from a CSV file
-
-     This function reads in station information from a CSV file and populates the stations unordered_map with Station objects.
-
-     @return true if the file was successfully opened and the data was loaded, false otherwise
-
-     @complexity O(n), where n is the number of stations in the CSV file
- */
     bool loadStations();
 
-    /**
-    @brief Loads the connection information from a CSV file
-
-    This function reads in connection information from a CSV file and populates the connections vector and targets unordered_map with Connection objects.
-
-    @return true if the file was successfully opened and the data was loaded, false otherwise
-
-    @complexity O(m), where m is the number of connections in the CSV file
- */
     bool loadConnections();
 
-    /**
-    @brief Returns the stations unordered_map
-
-    This function returns the unordered_map of stations, with the station name as the key and the Station object as the value.
-
-    @return the stations unordered_map
-
-    @complexity O(1)
- */
     unordered_map<string, Station> getStations();
 
-    /**
-    @brief Returns the connections vector
-
-    This function returns the vector of connections.
-
-    @return the connections vector
-
-    @complexity O(1)
- */
     vector<Connection> getConnections();
 
-    /**
-    @brief Returns the targets unordered_map
-
-    This function returns the unordered_map of targets, with the station name as the key and a vector of Connection objects as the value.
-
-    @return the targets unordered_map
-
-    @complexity O(1)
- */
     unordered_map<string, vector<Connection>> getTargets();
 
-    /**
-    @brief Performs a depth-first search from the source station to the destination station
-
-    This function performs a depth-first search from the source station to the destination station using a recursive helper function.
-
-    @param source the name of the source station
-    @param destination the name of the destination station
-
-    @return true if there exists a path from the source station to the destination station, false otherwise
-
-    @complexity O(m), where m is the number of connections in the graph
- */
     bool bfs(string source, string destination);
 
-    /**
-    @brief Recursive helper function for depth-first search
-
-    This function is a recursive helper function for the dfs() function. It performs a depth-first search from the source station to the destination station.
-
-    @param source the name of the source station
-    @param destination the name of the destination station
-    @param visited an unordered_map of visited stations
-
-    @return true if there exists a path from the source station to the destination station, false otherwise
-
-    @complexity O(m), where m is the number of connections in the graph
- */
     bool dfsHelper(const string &source, const string &destination, unordered_map<string, bool> &visited);
 
-    /**
-    @brief Performs a breadth-first search from the source station to the destination station
-
-    This function performs a breadth-first search from the source station to the destination station using a queue.
-
-    @param source the name of the source station
-    @param destination the name of the destination station
-
-    @return true if there exists a path from the source station to the destination station, false otherwise
-
-    @complexity O(m + n log n), where m is the number of connections in the graph and n is the number of stations in the graph
- */
     bool dfs(string source, string destination);
 
-    /**
-    @brief Calculates the shortest path between two nodes using Dijkstra's algorithm.
-    @param source The starting node.
-    @param destination The target node.
-    @return A boolean indicating if a path was found.
-    @complexity O(E*log(V)), where E is the number of edges and V is the number of vertices.
-*/
     bool dijkstra(string source, string destination);
 
-    /**
-    @brief Finds an augmenting path in the residual graph using DFS.
-    @param source The source node.
-    @param sink The sink node.
-    @param flow The flow so far in the augmenting path.
-    @param visited A map of visited nodes.
-    @return The flow of the augmenting path.
-    @complexity O(E*log(V)), where E is the number of edges and V is the number of vertices.
-*/
-    int findAugmentingPath(const string& source, const string& sink, int flow, unordered_map<string, bool>& visited);
+    int findAugmentingPath(const string &source, const string &sink, int flow, unordered_map<string, bool> &visited);
 
-    /**
-    @brief Calculates the maximum flow in the graph using the Edmonds-Karp algorithm.
-    @param source The source node.
-    @param sink The sink node.
-    @return The maximum flow.
-    @complexity O(V*E^2), where E is the number of edges and V is the number of vertices.
-*/
     int calculateMaxFlow(string source, string sink);
 
-    /**
-    @brief Finds all pairs of stations with the highest maximum flow from a given source station.
-    @param source The source station.
-    @param sink The sink station.
-    @return A vector of station pairs.
-    @complexity O(V^2*E^2), where E is the number of edges and V is the number of vertices.
-*/
     vector<pair<string, string>> highestMaxFlowPairsPath(string source, string sink);
 
-    /**
-    @brief Calculates the maximum number of trains that can be parked at a station.
-    @param station The station.
-    @return The maximum number of trains that can be parked.
-    @complexity O(V*E^2), where E is the number of edges and V is the number of vertices.
-*/
     int maxTrainsAtStation(string stationName);
 
-    /**
-    @brief Finds all source stations in the graph.
-    @return A vector of source stations.
-    @complexity O(V+E), where E is the number of edges and V is the number of vertices.
-*/
     vector<string> findSourceStations();
 
-    /**
-    @brief Finds all pairs of stations with the highest maximum flow.
-    @return A vector of station pairs.
-    @complexity O(V^3*E^2), where E is the number of edges and V is the number of vertices.
-*/
     vector<pair<string, string>> highestMaxFlowPairs();
 
-    /**
-    @brief Updates the residual capacities of the connections in the graph.
-    @complexity O(E), where E is the number of edges in the graph.
-*/
     void updateResidualConnections();
 
-    /**
-    @brief Calculates the minimum cost maximum flow from a source to a sink using Dijkstra's algorithm and updates the residual capacities of the graph.
-    @param source The name of the source node.
-    @param sink The name of the sink node.
-    @return A pair of integers representing the maximum flow and minimum cost of the graph.
-    @note The time complexity of this function is O(E^2 * log(V)), where E is the number of edges and V is the number of vertices in the graph.
- */
     pair<int, int> calculateMinCostMaxFlow(string source, string sink);
 
-
-    /**
-    @brief Calculates the maximum flow that can be sent from a source to a sink using the Ford-Fulkerson algorithm, considering only nodes that belong to the same municipality as the source node.
-
-    @param source The name of the source node.
-    @param sink The name of the sink node.
-    @return The maximum flow that can be sent from the source to the sink, considering only nodes that belong to the same municipality as the source node.
-    @note The time complexity of this function is O(E * F), where E is the number of edges and F is the maximum flow in the graph.
- */
     int calculateMaxFlowMunicipality(string source, string sink);
 
-
-    /**
-    @brief Performs a breadth-first search (BFS) on the graph from a source to a destination, visiting only nodes that belong to the same municipality as the source node.
-
-    @param source The name of the source node.
-    @param destination The name of the destination node.
-    @return true if a path exists between the source and destination nodes that visits only nodes in the same municipality, false otherwise.
-    @note The time complexity of this function is O(E), where E is the number of edges in the graph.
- */
     bool bfsMunicipality(string source, string destination);
 
-
-    /**
-    @brief Returns the top k municipalities with the highest maximum flow that can be sent from a station in that municipality to any other station within the same municipality.
-    @return A vector of strings containing the names of the top k municipalities with the highest maximum flow.
-    @note The time complexity of this function is O(E * V * F * log(V)), where E is the number of edges, V is the number of vertices, and F is the maximum flow in the graph.
- */
     vector<string> topkbudgetMunicipality();
-
 
     int calculateMaxFlowDistrict(string source, string sink);
 
-    /**
-    @brief Performs a breadth-first search (BFS) on the graph from a source to a destination, visiting only nodes that belong to the same district as the source node.
-
-    @param source The name of the source node.
-    @param destination The name of the destination node.
-    @return true if a path exists between the source and destination nodes that visits only nodes in the same district, false otherwise.
-    @note The time complexity of this function is O(E), where E is the number of edges in the graph.
- */
     bool bfsDistrict(string source, string destination);
 
-/**
-    @brief Finds the district(s) with the highest maximum flow capacity between its stations.
-    @return A vector of the name(s) of the district(s) with the highest maximum flow capacity.
-    @complexity O(E * V^2), where E is the number of edges and V is the number of vertices (stations)
- */
     vector<string> topkbudgetDistrict();
 
-    /**
-    @brief Removes the connection between two stations.
-    @param s1 The name of the first station.
-    @param s2 The name of the second station.
-    @complexity O(E), where E is the number of edges
- */
+    //pair<int, int> calculateMinCostMaxFlow(string source, string sink);
+
+
     void removeConnection(string s1, string s2);
 };
 
