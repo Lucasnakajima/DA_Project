ec96529283b6b232d3466ad24e884711f5ae2d3f
Lucas Nakajima <nakajima.lucas02@gmail.com>
2023-04-10 08:56:18 +0100

    Doxy fixed

--- Doxygen/html/navtreedata.js
+++ Doxygen/html/navtreedata.js
@@ -24,7 +24,7 @@
 */
 var NAVTREE =
 [
-  [ "AED Project1 G03_1", "index.html", [
+  [ "DA Project1 G03_1", "index.html", [
     [ "Classes", "annotated.html", [
       [ "Class List", "annotated.html", "annotated_dup" ],
       [ "Class Index", "classes.html", null ],
